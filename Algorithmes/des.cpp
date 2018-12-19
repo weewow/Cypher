583e1da5c7c02999f4fbb2e9fb8f2f4767b8c9dc
Weewow <gillian.chevailler@gmail.com>
2018-12-19 17:56:43 +0100

    suite implémentation, génération de la clé 48bits à partir de la clé 64bits, début de modification d'un bloc via la clé 48bits

--- Algorithmes/des.cpp
+++ Algorithmes/des.cpp
@@ -1,11 +1,75 @@
 #include "des.h"
-//#include <cassert>
+#include <cassert>
 #include <QString>
 #include <QVector>
 #include <QBitArray>
 #include <sstream>
 
 namespace DES {
+    // Table permettant de générer une clé de 56 bits à partir d'une clé de 64 bits
+    // par exemple, le premier bit de la clé arrondis sera le 57ème bit de la clé de base
+    // trouvé sur http://desimplementation.blogspot.com/
+    int CleArrondi[56]={57, 49, 41, 33, 25, 17, 9,
+                         1, 58, 50, 42, 34, 26, 18,
+                        10, 2,  59, 51, 43, 35, 27,
+                        19, 11, 3,  60, 52, 44, 36,
+                        63, 55, 47, 39, 31, 23, 15,
+                        7,  62, 54, 46, 38, 30, 22,
+                        14, 6,  61, 53, 45, 37, 29,
+                        21, 13, 5,  28, 20, 12, 4};
+
+    // Tableau des 8 tables de substitution utilisées par DES
+    int sBox[8][64]=
+        {
+            {
+                14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,
+                0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
+                4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
+                15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13
+            },
+            {
+                15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
+                3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
+                0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
+                13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9
+            },
+            {
+                10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
+                13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
+                13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
+                1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12
+            },
+            {
+                7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
+                13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
+                10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
+                3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14
+            },
+            {
+                2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
+                14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
+                4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
+                11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3
+            },
+            {
+                12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
+                10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
+                9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
+                4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13
+            },
+            {
+                4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
+                13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
+                1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
+                6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12
+            },
+            {
+                13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
+                1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
+                7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
+                2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11
+            }
+        };
 
     /**
         @name DecoupeText
@@ -14,7 +78,6 @@ namespace DES {
         @param textClair texte à découper de taille quelconque
         @param blocVector vector dans lequel on stock les blocs de 64 bits
     */
-    // TODO gestion du dernier bloc si elui-ci ne fait pas 64 bits
     void DecoupeText(QBitArray const &textClair, QVector<QBitArray> & blocVector)
     {
         QBitArray bloc(64, false);
@@ -52,28 +115,6 @@ namespace DES {
     }
 
     /**
-        @name DecoupeDemiBlocs
-        Découpe le bloc blocOrigine en un demi bloc gauche et un demi bloc droit
-
-        @param blocOrigine le bloc à diviser en deux, taille 64 bits
-        @param blocGauche partie gauche du bloc d'origine, taille 32 bits
-        @param blocDroit partie droite du bloc d'origine, taille 32 bits
-    */
-    // TODO faire un assert sur la taille des paramètres ?
-    void DecoupeDemiBlocs(QBitArray const &blocOrigine, QBitArray &blocGauche, QBitArray &blocDroit)
-    {
-        for(int i=0; i<blocOrigine.size(); i++)
-        {
-            if(i<32)
-            {
-                blocGauche[i] = blocOrigine[i];
-            }else{
-                blocDroit[i-32] = blocOrigine[i];
-            }
-        }
-    }
-
-    /**
         @name ExtraireBits
         extrait taille bits de source à partir de l'index posDepart
 
@@ -82,15 +123,48 @@ namespace DES {
         @param taille nombre de bits à extraire
         @return les taille premiers bits de source à partir de l'index posDepart
     */
-    // TODO assert si posDepart+taille < source.size ?
     QBitArray ExtraireBits(QBitArray const & source, int posDepart, int taille)
     {
+        assert(("l'élément source est trop petit ",posDepart+taille > source.size()));
+        //
         QBitArray result(taille);
         for(int i=0; i< taille; i++)
         {
             result[i] = source[i+posDepart];
         }
         return result;
+    }
+
+    /**
+        @name DeterminerCle48bits
+        Convertis une clé de 64 bits en une clé de 48 bits
+
+        @param cle la clé de 64 bits à convertir
+    */
+    QBitArray DeterminerCle48bits(QBitArray const & cle)
+    {
+        QBitArray result(48);
+        for(int i=0; i<48; i++)
+        {
+            result[i] = cle[CleArrondi[i]];
+        }
+
+        return result;
+    }
+
+    /**
+        @name f
+        coeur de l'algorithme DES, altère le bloc bloc grâce à la clé passée en paramètre
+
+        @param bloc le bloc de données à altérer (64 bits)
+        @param cle une clé de 48 bits
+    */
+    void f(QBitArray  &bloc, QBitArray const &cle)
+    {
+        // Décomposer la clé en deux sous parties
+        QBitArray cleGauche(ExtraireBits(cle, 0, 24)),
+                  cleDroit(ExtraireBits(cle, 24, 24));
+        // TODO
     }
 
     /**
@@ -105,13 +179,16 @@ namespace DES {
     {
         QString result("");
         QBitArray blocGauche(32), blocDroit(32);
-        // tester si la clé fait 56 bits, faire un asert?
-        if (cle.size() == 56)
+        // tester si la clé fait 64 bits, faire un asert?
+        if (cle.size() == 64)
         {
             // découper le message en blocs de 64 bits
             QVector<QBitArray> blocVector;
             QVector<std::pair<QBitArray, QBitArray>> blocVectorGD;
             DecoupeText(textClair, blocVector);
+
+            // Génération d'une clé de 48 bits
+            QBitArray cle48bits(DeterminerCle48bits(cle));
 
             // pour chaque bloc faire
             for(int i=0; i<blocVector.size(); i++)
@@ -122,20 +199,23 @@ namespace DES {
                 // diviserle bloc en un bloc gauche et un bloc droit de 32 bits
                 blocGauche.fill(false);
                 blocDroit.fill(false);
-                DecoupeDemiBlocs(blocVector[i], blocGauche, blocDroit);
+                blocGauche = ExtraireBits(blocVector[i], 0, 32);
+                blocDroit = ExtraireBits(blocVector[i], 32, 32);
                 blocVectorGD.append(std::pair<QBitArray, QBitArray>(blocGauche, blocDroit));
+
                 if(i>0)
                 {
                     // faire 16 fois ce qui suit
                     for(int j=0; j<16; j++)
                     {
-                        QBitArray cle48bits(ExtraireBits(cle, j, 48));
                         // le bloc gauche devient le bloc droit de l'itération précédente
                         std::swap(blocVectorGD[i].first, blocVectorGD[i-1].second);
                         // le bloc droit devient le boc gauche de l'itération précédente XOR f(Di-1, Ki)
-//                        blocVectorGD[i].second = blocVectorGD[i-1].first ^ f(blocVectorGD[i-1].second, cle48bits);
+                        f(blocVectorGD[i-1].second, cle48bits);
+                        blocVectorGD[i].second = blocVectorGD[i-1].first ^ blocVectorGD[i-1].second;
                     }
                     // On recompose un bloc B'16 en "recollant" D16 et G16 dans cet ordre
+                    // TODO
                 }
             }
         }
